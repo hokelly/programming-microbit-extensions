bee3def45ab011fca08090f9ac5fed32443bb04c
hokelly <32305411+hokelly@users.noreply.github.com>
2023-04-25 20:45:02 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+//% color = "#12345" weight=100
+namespace HelenOKelly{
+
+    //% block
+    export function HELLO(){
+        basic.showString("HOK")
+    }
+
+    //% block
+    export function ADD(attribute1: number, attribute2: number){
+        let x = attribute1 + attribute2;
+        return x;
+    }
+}
